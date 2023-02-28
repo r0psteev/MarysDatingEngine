62c98a8a7bd52ad0c67d6d63536f5f4867a88a4b
r0psteev <r0psteev@github.com>
2023-02-28 16:10:31 -0500

    For some strange reason, some directory cleanup

--- golang.sh
+++
@@ -1,5 +0,0 @@
-pushd /tmp
-wget https://go.dev/dl/go1.19.5.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-source ~/.profile
